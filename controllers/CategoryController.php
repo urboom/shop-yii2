311eeb540154aeaed9a66b6eaf5db1eb4df6fe1a
Yurii <yurec116@gmail.com>
2017-02-19 21:57:14 +0200

    Add Site Search

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -12,22 +12,40 @@ class CategoryController extends AppController
     {
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
         $this->setMeta('E-SHOPPER');
+
         return $this->render('index', compact('hits'));
     }
 
     public function actionView($id)
     {
-        $id = Yii::$app->request->get('id');
         $category = Category::findOne($id);
+
         if(empty($category)) {
             throw new \yii\web\HttpException(404, 'This category NOT FOUND');
         }
-        //$products = Product::find()->where(['category_id' => $id])->all();
+
         $query = Product::find()->where(['category_id' => $id]);
         $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 6, 'forcePageParam' => false, 'pageSizeParam' => false]);
         $products = $query->offset($pages->offset)->limit($pages->limit)->all();
         $category = Category::findOne($id);
         $this->setMeta('E-SHOPPER | ' . $category->name, $category->keywords, $category->description);
+
         return $this->render('view', compact('products', 'pages', 'category'));
+    }
+
+    public function actionSearch()
+    {
+        $qr = trim(Yii::$app->request->get('qr'));
+        $this->setMeta('E-SHOPPER | Поиск: ' . $qr);
+
+        if(!$qr) {
+            return $this->render('search');
+        }
+
+        $query = Product::find()->where(['like', 'name', $qr]);
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 6, 'forcePageParam' => false, 'pageSizeParam' => false]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
+
+        return $this->render('search', compact('products', 'pages', 'qr'));
     }
 } 
