ca6b468b69d4a169061fc028465d463a4408d5f8
Yurii <yurec116@gmail.com>
2017-02-22 09:19:54 +0200

    Add product to cart (modal)

--- web/js/main.js
+++ web/js/main.js
@@ -2,9 +2,77 @@
 
  $('#sl2').slider();
 
-	 $('.catalog').dcAccordion({
-		 speed: 300
-	 });
+    $('.catalog').dcAccordion({
+        speed: 300
+    });
+
+    function showCart(cart){
+        $('#cart .modal-body').html(cart);
+        $('#cart').modal();
+    }
+
+    function getCart(){
+        $.ajax({
+            url: '/cart/show',
+            type: 'GET',
+            success: function(res){
+                if(!res) alert('Ошибка!');
+                showCart(res);
+            },
+            error: function(){
+                alert('Error!');
+            }
+        });
+        return false;
+    }
+
+    $('#cart .modal-body').on('click', '.del-item', function(){
+        var id = $(this).data('id');
+        $.ajax({
+            url: '/cart/del-item',
+            data: {id: id},
+            type: 'GET',
+            success: function(res){
+                if(!res) alert('Ошибка!');
+                showCart(res);
+            },
+            error: function(){
+                alert('Error!');
+            }
+        });
+    });
+
+    function clearCart(){
+        $.ajax({
+            url: '/cart/clear',
+            type: 'GET',
+            success: function(res){
+                if(!res) alert('Ошибка!');
+                showCart(res);
+            },
+            error: function(){
+                alert('Error!');
+            }
+        });
+    }
+
+    $('.add-to-cart').on('click', function (e) {
+        e.preventDefault();
+        var id = $(this).data('id'),
+            qty = $('#qty').val();
+        $.ajax({
+            url: '/cart/add',
+            data: {id: id, qty: qty},
+            type: 'GET',
+            success: function(res){
+                if(!res) alert('Ошибка!');
+                showCart(res);
+            },
+            error: function(){
+                alert('Error!');
+            }
+        });
+    });
 
 	var RGBChange = function() {
 	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
